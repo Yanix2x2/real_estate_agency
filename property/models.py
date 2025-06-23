62e4663344e68ca1c4e95b84b0ca355c91f51cef
Jana <yanix2x2@gmail.com>
2025-06-23 20:24:51 +0300

    transferred fields owners to model Owner

--- property/models.py
+++ property/models.py
@@ -81,3 +81,18 @@ class Complaint(models.Model):
         null=True
     )
     text = models.TextField('Текст жалобы')
+
+
+class Owner(models.Model):
+    owner_name = models.CharField('ФИО владельца', max_length=200)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True)
+    flats = models.ManyToManyField(
+        Flat,
+        related_name='owners',
+        verbose_name='Квартиры в собственности',
+        blank=True
+    )
+
+    def __str__(self):
+        return f'{self.owner_name}'
