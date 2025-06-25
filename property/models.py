39a955e0c2a7b1fddc46f6f9ab724d091c0620b6
Jana <yanix2x2@gmail.com>
2025-06-25 22:32:13 +0300

    renamed fields

--- property/models.py
+++ property/models.py
@@ -81,9 +81,9 @@ class Complaint(models.Model):
 
 
 class Owner(models.Model):
-    owner_name = models.CharField('ФИО владельца', max_length=200, db_index=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owner_pure_phone = PhoneNumberField(
+    name = models.CharField('ФИО владельца', max_length=200, db_index=True)
+    phonenumber = models.CharField('Номер владельца', max_length=20)
+    pure_phone = PhoneNumberField(
         'Нормализованный номер владельца',
         blank=True, 
         db_index=True
